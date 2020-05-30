8aa503100d908b1e42bcbc02aebe7e273ef78c03
Носенко Нікіта <nikitanosenko3@gmail.com>
2020-05-30 17:48:07 +0300

    Лаба 1

---
+++ Lab__OOP_MyFrac/MyFrac.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab__OOP_MyFrac
+{
+    class MyFrac
+    {
+        long a; 
+        long b;
+        public MyFrac(long a_, long b_)
+        {
+            long nsd = AlgEuclid(Math.Abs(a_), Math.Abs(b_)); 
+            a = a_ / nsd;
+            b = b_ / nsd;
+            if (a < 0 && b < 0)
+            {
+                a = -a;
+                b = -b;
+            }
+            else if (b < 0)
+            {
+                a = -a;
+                b = -b;
+            }
+        }
+        private long AlgEuclid(long a, long b)            
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+            return Math.Max(a, b);
+        }
+        public override string ToString()              //Формує рядкове подання дробу, у вигляді, наприклад "17/7".
+        {
+            return a + "/" + b;
+        }
+        public  string ToStringWithIntegerPart()          //рядковее подання дробу з виділенням цілої частини.
+        {
+            if (a > 0)
+                return "(" + a / b + " + " + a % b + "/" + b + ")"; 
+            else
+            {
+                return "-(" + a / b + " + " + -a % b + "/" + b + ")";
+            }
+        }
+        public double DoubleValue()                      //формує дійсне значення дробу.
+        {
+            return 1.0 * a / b;
+        }
+        public static MyFrac operator +(MyFrac f1, MyFrac f2)  //обраховує суму дробів.
+        {
+            return new MyFrac(f1.a * f2.b + f1.b * f2.a, f1.b * f2.b);
+        }
+
+        public static MyFrac operator -(MyFrac f1, MyFrac f2)   //обраховує різницю дробів.
+        {
+            return new MyFrac(f1.a * f2.b - f1.b * f2.a, f1.b * f2.b);
+        }
+
+        public static MyFrac operator *(MyFrac f1, MyFrac f2)    //обраховує множення дробів.
+        {
+            return new MyFrac(f1.a * f2.a, f1.b * f2.b);
+        }
+
+        public static MyFrac operator /(MyFrac f1, MyFrac f2)   //обраховує ділення дробів.
+        {
+            return new MyFrac(f1.a * f2.b, f1.b * f2.a);
+        }
+
+        static public MyFrac GetRGR113LeftSum(long n)           
+        {
+            MyFrac ans = new MyFrac(1, (2 * 1 - 1) * (2 * 1 + 1));
+            for (int i = 2; i <= n; i++)
+            { ans += new MyFrac(1, (2 * i - 1) * (2 * i + 1)); }
+            return ans;
+        }
+  
+        static public MyFrac GetRGR115LeftSum(long n)         
+        {
+            MyFrac ans = new MyFrac(1, 1);
+            for (int i = 2; i <= n; i++)
+            { ans *= new MyFrac(1, 1) - new MyFrac(1, (i * i)); }
+            return ans;
+        }
+    }
+}
---
+++ Lab__OOP_MyFrac/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab__OOP_MyFrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть чисельник дробу:");
+            long n = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Введiть знаменник дробу:");
+            long k = Convert.ToInt64(Console.ReadLine());
+            MyFrac f = new MyFrac(n, k);
+            Console.WriteLine(f);
+            Console.WriteLine("Введiть чисельник дробу, який використовуватиметься для арифметичних дiй:");
+            n = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Введiть знаменник дробу, який використовуватиметься для арифметичних дiй:");
+            k = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Введiть число n для GetRGR113LeftSum:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введiть число n для GetRGR115LeftSum:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            MyFrac f2 = new MyFrac(n, k);
+            Console.WriteLine(f2);
+            Console.WriteLine("Перший дрiб у форматi MyFrac:\n" + f);
+            Console.WriteLine("Другий дрiб у форматi MyFrac:\n" + f2);
+            Console.WriteLine("Перший дрiб з видiленою цiлою частиною:\n" + f.ToStringWithIntegerPart());
+            Console.WriteLine("Другий дрiб з видiленою цiлою частиною:\n" + f2.ToStringWithIntegerPart());
+            Console.WriteLine("Дiйсне значення першого дробу:\n" + f.DoubleValue());
+            Console.WriteLine("Дiйсне значення другого дробу:\n" + f2.DoubleValue());
+            Console.WriteLine("Сума дробiв:\n" + (f + f2));
+            Console.WriteLine("Рiзниця дробiв:\n" + (f - f2));
+            Console.WriteLine("Добуток дробiв:\n" + (f * f2));
+            Console.WriteLine("Частка дробiв:\n" + (f / f2));
+            Console.WriteLine("GetRGR113LeftSum:\n" + MyFrac.GetRGR113LeftSum(n1));
+            Console.WriteLine("GetRGR115LeftSum:\n" + MyFrac.GetRGR115LeftSum(n2));
+            Console.ReadKey();
+        }
+    }
+}
